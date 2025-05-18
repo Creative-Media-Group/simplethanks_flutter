6e6cf62ef30cb9403f978ef222d44e936ccb8e3a
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-19 01:44:45 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/simplethanks/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.simplethanks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse('https://flutter.dev');
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Simple Thanks")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 5,
+            //crossAxisAlignment: CrossAxisAlignment.baseline,
+            children: [
+              ElevatedButton(onPressed: btnpressed, child: Text("hello")),
+              ElevatedButton(
+                onPressed: _launchUrl,
+                child: Text("Open Website"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void btnpressed() {
+  if (kDebugMode) {
+    print("Hello");
+  }
+}
+
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw Exception('Could not launch $_url');
+  }
+}

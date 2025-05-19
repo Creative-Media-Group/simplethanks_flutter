843159768962516b94c7a224be6665c42a8721b3
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-19 03:54:12 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +8,14 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.simplethanks"
+    namespace = "de.cmg.simplethanks"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
